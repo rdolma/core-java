ba7719ec210277ae454417f449ab9b243c0d45fb
Tashi Palmo <tashipalmo@Tashis-Air.lan>
2024-09-26 00:08:02 -0400

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    public static void main(String[] args) {
+        String name="Rinzin";
+        int age= 45;
+        String dept="Operation";
+        double salary=600.4;
+        String address="Maple dr. CT 56780";
+        long phone_number=244332548;
+
+        System.out.println("This employee just joined the firm recently. Please help her.");
+    }
+}
---
+++ src/loops/ForLoop.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int num1=5;
+        int num2=10;
+        for (int i=1; i<=10; i++)
+        {
+            System.out.println(num1 * i);
+        }
+    }
+}
